1c5fedce077c786e7c3b78d4be6ec3955a8e395a
trisuli <trisulisrilakshmi17@gmail.com>
2021-07-14 23:47:46 +0530

    last push

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>User CRUD With Bootstrap And PHP</title>
+        <link href="assets/vendors/bootstrap/css/bootstrap.css">
+        
+    </head>
+    <body>
+        
+    </body>
 </html>
-<html>
